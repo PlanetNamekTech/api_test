e3182dca0246a5043971e6527feee00ddf5d0b8b
PlanetNamekTech <sr23513@gmail.com>
2022-09-04 15:25:47 -0700

    CRUD flask api basic test

--- api_test.py
+++ api_test.py
@@ -1,6 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
@@ -19,4 +20,39 @@ def index():
 
 @app.route('/drinks')
 def get_drinks():
-  return {"drinks": "drink data"}
+  drinks = Drink.query.all()
+
+  output = []
+  for drink in drinks:
+    drink_data = {'name': drink.name, 'description': drink.description}
+
+    output.append(drink_data)
+  return {"drinks": output}
+
+@app.route('/drinks/<id>')
+def get_drink(id):
+  drink = Drink.query.get_or_404(id)
+  return {"name": drink.name, "description": drink.description}
+
+@app.route('/drinks', methods=['POST'])
+def add_drink():
+  drink = Drink(name=request.json['name'], description=request.json['description'])
+  db.session.add(drink)
+  db.session.commit()
+  return  {'id': drink.id}
+
+@app.route('/drinks/<id>/edit', methods=['PUT'])
+def update_drink(id):
+  drink = Drink.query.get(id)
+  drink.description = request.json.get('description', drink.description)
+  db.session.commit()
+  return {"Message":"Drink successfully updated."}
+
+@app.route('/drinks/<id>', methods=['DELETE'])
+def delete_drink(id):
+  drink = Drink.query.get(id)
+  if drink is None:
+    return {"error": "Not found"}
+  db.session.delete(drink)
+  db.session.commit()
+  return {"Message": "Drink successfully deleted."}
